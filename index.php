22a8b03e0a972abd94a35ae3a21e0439e1258ab3
stephane.gibault36@gmail.com <stephane.gibault36@gmail.com>
2015-08-16 12:35:57 +0200

    basic stuff modif

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 	
 <title>BRAINMAP - Schémas réseaux</title>
 <meta charset="UTF8"></meta>
-<meta http-equiv="X-UA-Compatible" content="IE=10" />
-<meta name="viewport" content="width=device-width, height=device-height">
+<meta http-equiv="X-UA-Compatible" content="IE=9,chrome=1" />
+<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
 
 <script src="Plugins/jquery.js" type="text/javascript"></script>
 <script src="Plugins/jquery-ui.js" type="text/javascript"></script>
